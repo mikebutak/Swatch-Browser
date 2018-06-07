f84fb7ca9a374872a560a873461b9e3c55aa6251
Michael Butak <mikebutak@gmail.com>
2018-06-07 08:52:24 -0700

    style header component, import svg as component

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import classes from './Header.css';
+import Logo from '../../assets/logo-symbol';
 
 const header = () => (
-    <div>
-        <p>Header compiling properly</p>
-    </div>
+    <nav className="navbar navbar-dark bg-dark">
+        <div className="container-fluid">
+            <div className="navbar-header">
+                <a className="navbar-brand" href="http://www.clker.com/cliparts/Z/W/N/F/6/p/light-blue-square-md.png">
+                    <Logo />
+                </a>
+            </div>
+        </div>
+    </nav>
 )
 
 export default header;
