d062ca9cdf88f51a15c4534fef50cd5f56cb195b
Michael Butak <mikebutak@gmail.com>
2018-06-09 07:10:25 -0700

    add search input to header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -5,10 +5,21 @@ import './Header.css';
 const header = () => (
     <nav className="navbar navbar-dark bg-dark height">
         <div className="container-fluid">
-            <div className="navbar-header">
-                <a className="navbar-brand" href="http://www.helpfulhuman.com/">
-                    <Logo />
-                </a>
+            <div className="navbar-header nav-wide">
+                <div className="row justify-content-end">
+                    <div className="col">
+                        <a className="navbar-brand" href="http://www.helpfulhuman.com/">
+                            <Logo />
+                        </a>
+                    </div>
+                    <div className="col search-style">
+                        <form>
+                            <div className="form-group form-adj">
+                                <input type="text" className="form-control" placeholder="Search" />
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     </nav>
