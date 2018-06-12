7c812b1b973d048df5eb470dd2f7ca93555425d3
Michael Butak <mikebutak@gmail.com>
2018-06-11 22:39:04 -0700

    revert env setting to hardcoded var

--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const config = require('../knexfile');
-const env = process.env.NODE_ENV;
+const env = 'production';
 const knex = require('knex')(config[env]);
 
 module.exports = knex;
