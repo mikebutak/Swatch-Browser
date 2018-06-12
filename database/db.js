c28a05654a9a0f55d4b5b28b3815ad24dc561f2e
Michael Butak <mikebutak@gmail.com>
2018-06-11 21:56:26 -0700

    cleanup db.js

--- database/db.js
+++ database/db.js
@@ -1,30 +1,3 @@
-// require('dotenv').config();
-
-// const knex = require('knex')({
-//     client: 'mysql',
-//     connection: {
-//         host: process.env.DATABASE_URL || '127.0.0.1',
-//         user: process.env.DATABASE_USER,
-//         insecureAuth: true,
-//         password: process.env.DATABASE_PASSWORD,
-//         database: process.env.DATABASE_NAME,
-//     },
-//   });
-
-// knex.schema.hasTable('color').then((exists) => {
-//     if (!exists) {
-//       return knex.schema.createTable('color', (table) => {
-//         table.increments('id').primary();
-//         table.string('hex', 100);
-//         table.timestamp('created_at').notNullable().defaultTo(knex.raw('now()'));
-//       }).then(()=> {
-//         console.log('created user table!'
-//       )});
-//     }
-//   })
-  
-//   module.exports = knex;
-
 const config = require('../knexfile');
 const env = 'production';
 const knex = require('knex')(config[env]);
